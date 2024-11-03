85c5491adda71a844fdbb1b3b6fd1cd8ac478c6a
Remgrad <remgrad622@gmail.com>
2024-11-03 23:25:01 +0300

    Матодя строк

---
+++ main.py
@@ -0,0 +1,8 @@
+my_string = (input('Введите ФИО: '))
+print(my_string)
+print(len(my_string), "символов") # количество символов введённого текста
+print(my_string.upper()) # верхний регистр
+print(my_string.lower()) # нижний регистр
+print(my_string.replace(' ','')) # без пробела
+print(my_string[0]) # Первый символ
+print(my_string[-1]) # Последний символ
