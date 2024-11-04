423762a483d1735fbef1ed873dc8e3ace9778abd
Remgrad <remgrad622@gmail.com>
2024-11-04 22:34:49 +0300

    Неизменяемые и изменяемые объекты. Кортежи

--- module_1_5.py
+++ module_1_5.py
@@ -1,14 +1,11 @@
-# Задайте переменные разных типов данных:
-#   - Создайте переменную immutable_var и присвойте ей кортеж из нескольких элементов разных типов данных.
-#   - Выполните операции вывода кортежа immutable_var на экран.
 immutable_var = [1, 2], '3', 'дом', True
 print(immutable_var)
 #print(type(immutable_var))
-# Изменение значений переменных:
-# Попытайтесь изменить элементы кортежа immutable_var. Объясните, почему нельзя изменить значения элементов кортежа.
 # immutable_var[1][1] = 200
 # print(immutable_var) # TypeError: 'str' object does not support item assignment - кортеж не поддерживает обращение по элементам
 immutable_var[0][0] = 2
 print(immutable_var) # Допустимо изменение кортежа внутри которого есть список
 mutable_list = [1, 2, '3', 'дом', True]
+print(mutable_list)
+mutable_list[3] = 'здание'
 print(mutable_list)
