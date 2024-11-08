c77d032e33e71334f85c67621fe09d40e052d0c7
Remgrad <remgrad622@gmail.com>
2024-11-09 01:33:37 +0300

    Цикл While

---
+++ module_2_3.py
@@ -0,0 +1,8 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+initial_value = 0
+while initial_value < len(my_list):
+    if my_list [initial_value] < 0:
+        break
+    print(my_list [initial_value])
+    initial_value += 1
+    continue
